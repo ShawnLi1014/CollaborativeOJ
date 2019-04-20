45c77228ff39ff50ce3048e6004777e4dbe87884
李天伦 <litianlun@litianlundeMacBook-Pro.local>
2019-04-21 00:47:52 +0800

    angular front end-routing

--- week1/oj-client/src/app/app.module.ts
+++ week1/oj-client/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { routing } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { ProblemListComponent } from './components/problem-list/problem-list.component';
+
+import { DataService } from './services/data.service';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProblemListComponent,
+    ProblemDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    routing
   ],
-  providers: [],
+  providers: [{
+    provide: 'data',
+    useClass: DataService
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
