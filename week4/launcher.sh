507c9bfaaba8d7ca36e81e010a7e979d0e5baf90
李天伦 <litianlun@litianlundeMacBook-Pro.local>
2019-05-21 10:04:56 +0800

    modified launcher

--- week4/launcher.sh
+++ week4/launcher.sh
@@ -1,8 +1,8 @@
-
+#!/bin/bash
 fuser -k 3000/tcp
 fuser -k 5000/tcp
 
-sudo service redis_6397 start
+service redis_6379 start
 cd ./oj-server
 npm install
 nodemon server.js &
@@ -10,12 +10,12 @@ cd ../oj-client
 npm install
 ng build --watch &
 cd ../executor
-pip2 install -r requirements.txt
-python2 executor_server.py &
+pip install -r requirements.txt
+python executor_server.py &
 
-echo "===================================="
-read -p "PRESS [ENTER] TO TERMINATE PROCESSES" PRESSKEY
+echo "=================================================="
+read -p "PRESS [ENTER] TO TERMINATE PROCESSES." PRESSKEY
 
 fuser -k 3000/tcp
 fuser -k 5000/tcp
-sudo service redis_6397 stop
+service redis_6379 stop
